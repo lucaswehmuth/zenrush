93e5cf08432c19f67290fc2ea3ef459a74fe7e6d
Lucas Wehmuth <lucas.wehmuth@gmail.com>
2026-03-27 01:16:02 -0300

    More debug test

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var total_shards_label: Label = $CanvasLayer/CenterContainer/VBoxContainer/TotalShardsLabel
 @onready var play_button: Button = $CanvasLayer/CenterContainer/VBoxContainer/PlayButton
 @onready var sandbox_button: Button = $CanvasLayer/CenterContainer/VBoxContainer/SandboxButton
+@onready var test_button: Button = $TestButton
 
 func _ready() -> void:
 	total_shards_label.text = "Shards: %d" % save_manager.total_shards
@@ -11,6 +12,8 @@ func _ready() -> void:
 	play_button.button_up.connect(_on_play_up)
 	sandbox_button.pressed.connect(_on_sandbox)
 	sandbox_button.button_up.connect(_on_sandbox_up)
+	test_button.pressed.connect(_on_test_pressed)
+	test_button.button_up.connect(_on_test_up)
 
 func _on_play() -> void:
 	print("play")
@@ -28,6 +31,12 @@ func _on_sandbox_up() -> void:
 	print("sandbox up")
 	get_tree().change_scene_to_file("res://scenes/sandbox.tscn")
 
+func _on_test_pressed() -> void:
+	print("test")
+	
+func _on_test_up() -> void:
+	print("test up")
+	
 func _input(event):
 	if event is InputEventScreenTouch:
 		print("TOUCH DETECTED:", event.position)
